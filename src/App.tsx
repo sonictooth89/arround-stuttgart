00ed4bb3262e0404049f3e523c01caae54515747
Malgorzata Nowak <nowak.em@interia.eu>
2022-11-12 17:35:16 +0100

    initial commit standard layout with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, Container, NavDropdown, Row, Col, Image, Button, Card } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <Navbar bg="dark" variant='dark' expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       </header>
+      <main>
+      <Container>
+      <Row className="px-4 my-5">
+        <Col sm={7}>
+          <Image src="https://http.cat/101" fluid rounded className=""/>
+        </Col>
+        <Col sm={5}>
+          <h1 className="font-weight-light">Tagline</h1>
+          <p className="mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, odio consectetur. Voluptatum porro alias nihil iusto, eius maiores fugiat labore quaerat beatae dolores quidem placeat minima eos aut assumenda ea!
+          </p>
+          <Button variant="outline-primary">Call to action</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Card className='text-center bg-secondary text-white my-5 py-4'>
+          <Card.Body>This is some text within a card body</Card.Body>
+        </Card>
+      </Row>
+      <Row>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://http.cat/202" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://http.cat/201" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://http.cat/403" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+      </main>
+      <footer className="py-5 my-5 bg-dark">
+        <Container className="px-4">
+          <p className="text-center text-white">
+            Copywrite & copy; Your Website 2022
+          </p>
+        </Container>
+      </footer>
     </div>
   );
 }
