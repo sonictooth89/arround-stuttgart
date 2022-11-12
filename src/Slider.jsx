da2b6a4b856082d96e2c2a490bf9efd3710a6b2c
Malgorzata Nowak <nowak.em@interia.eu>
2022-11-12 20:06:39 +0100

    slider with new images edited

---
+++ src/Slider.jsx
@@ -0,0 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel';
+import image1 from './img1.jpg';
+import image2 from './img2.jpg';
+import image3 from './IMG_5691.jpg'; 
+
+function Slider() {
+  return (
+    <Carousel variant='light' interval={5000}>
+      <Carousel.Item>
+        <img
+          src={image1}
+          className="d-block w-100"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Vineyard</h3>
+          <p>The vineyards arround Stuttgart city.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Max-Eyth-See</h3>
+          <p>The beautiful Lake in Stuttgart</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={image3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Stuttgart-West</h3>
+          <p>
+            View of the city center.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slider;
